e6250123a10093b8796ee21b6b5b99debed2dd44
Space Dinosaur <91723941+space-dino@users.noreply.github.com>
2024-05-15 11:57:58 +0300

    first commit

---
+++ GPTcli.py
@@ -0,0 +1,171 @@
+import socket
+import tkinter as tk
+from threading import Thread
+import cv2
+from PIL import Image, ImageTk
+import pyaudio
+import time
+import pickle
+import struct
+import numpy as np
+import lz4.frame
+import protocol4
+
+
+class Client:
+    def __init__(self):
+        self.FORMAT, self.CHANNELS, self.RATE, self.A_CHUNK, self.audio, self.in_stream, self.out_stream = self.setup_audio()
+        self.root, self.username, self.window, self.entry, self.index_label, self.fps_label, self.username_label = self.setup_gui()
+        self.video_socket, self.audio_socket, self.host, self.port = self.setup_network()
+        self.labels, self.vid_data, self.aud_data, self.my_index, self.up = self.setup_data()
+        self.vid = cv2.VideoCapture(0)
+        self.vid.set(3, 320)  # Reduce frame width
+        self.vid.set(4, 240)  # Reduce frame height
+        self.mainloop()
+
+    def setup_audio(self):
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 1
+        RATE = 44100
+        A_CHUNK = 1024
+        audio = pyaudio.PyAudio()
+        in_stream = audio.open(format=FORMAT, channels=CHANNELS,
+                               rate=RATE, input=True, frames_per_buffer=A_CHUNK)
+        out_stream = audio.open(format=FORMAT, channels=CHANNELS,
+                                rate=RATE, output=True, frames_per_buffer=A_CHUNK)
+
+        return FORMAT, CHANNELS, RATE, A_CHUNK, audio, in_stream, out_stream
+
+    def setup_gui(self):
+        root = tk.Tk()
+        root.withdraw()
+        username = ""
+        window = tk.Toplevel(root)
+        window.title("Enter Your Name")
+        tk.Label(window, text="Enter your name:").pack()
+        entry = tk.Entry(window)
+        entry.pack()
+        tk.Button(window, text="Join Meeting", command=self.submit).pack()
+        tk.Button(root, text="Close", command=self.close_connection).grid(row=0, column=1)
+        index_label = tk.Label(root, text="index")
+        index_label.grid(row=1, column=1)
+        fps_label = tk.Label(root, text="fps")
+        fps_label.grid(row=2, column=1)
+        username_label = tk.Label(root, text="username")
+        username_label.grid(row=3, column=1)
+
+        return root, username, window, entry, index_label, fps_label, username_label
+
+    def setup_data(self):
+        labels = []
+        vid_data = b''
+        aud_data = b''
+        my_index = 0
+        up = True
+
+        return labels, vid_data, aud_data, my_index, up
+
+    def setup_network(self):
+        video_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        audio_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = '127.0.0.1'
+        port = 9997
+
+        return video_socket, audio_socket, host, port
+
+    def submit(self):
+        self.username = self.entry.get()
+
+        self.window.destroy()
+        self.root.deiconify()
+        self.username_label.config(text=self.username)
+        self.video_socket.connect((self.host, self.port))
+        self.audio_socket.connect((self.host, self.port + 1))
+        print("Connected to server")
+        self.start_threads()
+
+    def close_connection(self):
+        self.in_stream.stop_stream()
+        self.in_stream.close()
+        self.out_stream.stop_stream()
+        self.out_stream.close()
+        self.audio.terminate()
+        self.up = False
+        print("Closing Connection")
+        self.vid.release()
+        self.video_socket.close()
+        self.audio_socket.close()
+        self.root.destroy()
+
+    def send_vid(self):
+        counter = 0
+        start_time = time.time()
+        fps = 0
+        while self.up:
+            ret, frame = self.vid.read()
+            if not ret:
+                continue
+            frame = cv2.flip(frame, 1)
+            _, encoded_frame = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 70])
+            compressed_frame = lz4.frame.compress(encoded_frame.tobytes())
+            protocol4.send_frame(self.video_socket, compressed_frame, 0, 0, self.my_index)
+            # self.send_data(self.video_socket, compressed_frame, self.my_index)
+
+            counter += 1
+            if (time.time() - start_time) >= 1:
+                fps = counter
+                counter = 0
+                start_time = time.time()
+            self.draw_GUI_frame(frame, self.my_index, f"{fps} fps")
+
+    def receive_vid(self):
+        while self.up:
+            frame_data, vid_data, index, self.my_index = protocol4.receive_frame(self.video_socket, self.vid_data)
+
+            if frame_data and index != self.my_index:
+                # if frame_data:
+                decompressed_frame = lz4.frame.decompress(frame_data)
+                frame = np.frombuffer(decompressed_frame, dtype=np.uint8)
+                frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+                self.draw_GUI_frame(frame, index)
+
+    def send_aud(self):
+        while self.up:
+            aud_frame = self.in_stream.read(self.A_CHUNK)
+            protocol4.send_frame(self.audio_socket, aud_frame, 0, 0, self.my_index)
+
+    def receive_aud(self):
+        while self.up:
+            aud_frame, aud_data, index, self.my_index = protocol4.receive_frame(self.audio_socket, self.aud_data)
+
+            if not aud_frame:
+                break
+            self.out_stream.write(aud_frame)
+
+    def draw_GUI_frame(self, frame, index, fps=None):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = Image.fromarray(frame)
+        frame = ImageTk.PhotoImage(image=frame)
+        while index >= len(self.labels):
+            label = tk.Label(self.root)
+            self.labels.append(label)
+        self.labels[index].grid(row=index, column=0)
+        self.labels[index].config(image=frame)
+        self.labels[index].image = frame
+        if index != -1:
+            self.index_label.config(text=f"client {index}")
+        if fps:
+            self.fps_label.config(text=fps)
+        self.root.update()
+
+    def start_threads(self):
+        Thread(target=self.send_vid).start()
+        Thread(target=self.send_aud).start()
+        Thread(target=self.receive_vid).start()
+        Thread(target=self.receive_aud).start()
+
+    def mainloop(self):
+        self.root.mainloop()
+
+
+Client()
---
+++ GPTser.py
@@ -0,0 +1,132 @@
+import socket
+from threading import Thread
+import protocol4
+import sqlite3
+from datetime import datetime
+
+vid_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+aud_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = '0.0.0.0'
+port = 9997
+vid_socket_address = (host, port)
+aud_socket_address = (host, port + 1)
+
+vid_server_socket.bind(vid_socket_address)
+aud_server_socket.bind(aud_socket_address)
+
+vid_server_socket.listen()
+aud_server_socket.listen()
+
+print(socket.gethostname())
+print("Listening video at", vid_socket_address, "audio at", aud_socket_address)
+
+vid_clients = []
+aud_clients = []
+
+
+class connection:
+    def __init__(self, soc: socket.socket, index: int):
+        self.soc = soc
+        self.frame = b''
+        self.data = b''
+        self.index = index
+
+
+def accept_connections(soc: socket.socket, lis):
+    while True:
+        client_socket, addr = soc.accept()
+        cli_index = int(str(addr[0]).replace(".", ""))
+        con = connection(client_socket, cli_index)
+        lis.append(con)
+
+        if lis == vid_clients:
+            print(f"GOT VIDEO CONNECTION FROM: ({addr[0]}:{addr[1]}) {cli_index}\n")
+            update_database(cli_index, addr[0])
+
+        if lis == aud_clients:
+            print(f"GOT AUDIO CONNECTION FROM: ({addr[0]}:{addr[1]}) {cli_index}\n")
+
+        Thread(target=handle_client, args=(con,)).start()
+
+
+def handle_client(con: connection):
+    """if con in aud_clients:
+        while True:
+            try:
+                data = con.soc.recv(1024 * 2)
+                if not data:
+                    break
+
+                for i in aud_clients:
+                    i.soc.sendall(data)
+            except ConnectionResetError:
+                remove_client(con, vid_clients)
+                remove_client(con, aud_clients)
+    else:"""
+    while True:
+        try:
+            con.frame, con.data, *_ = protocol4.receive_frame(con.soc, con.data)
+            if con in aud_clients:
+                for i in aud_clients:
+                    ipos = get_index_pos(i)
+                    cpos = get_index_pos(con)
+                    protocol4.send_frame(i.soc, con.frame, 0, cpos, ipos)
+            else:
+                for i in vid_clients:
+                    ipos = get_index_pos(i)
+                    cpos = get_index_pos(con)
+                    protocol4.send_frame(i.soc, con.frame, 0, cpos, ipos)
+        except ConnectionResetError:
+            remove_client(con, vid_clients)
+            remove_client(con, aud_clients)
+
+
+def get_index_pos(i):
+    sorted_numbers = sorted([conn.index for conn in vid_clients])
+    pos = sorted_numbers.index(i.index)
+    return pos
+
+
+def remove_client(con: connection, lis):
+    for i in lis:
+        if i.index == con.index:
+            print(f"Removing Connection {i.index}")
+            lis.remove(i)
+            sq = sqlite3.connect("video_chat.db")
+            cur = sq.cursor()
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            sql = f'UPDATE participant SET logout_time = "{current_time}" WHERE name = {i.index}'
+            cur.execute(sql)
+            sq.commit()
+            res = cur.execute("SELECT * FROM participant")
+            print("*****SQL******\n", res.fetchall())
+
+            for o in range(0, len(lis)):
+                if lis[o] is None:
+                    if o < len(lis) - 1:
+                        lis[o].index -= 1
+
+
+def update_database(name, ip):
+    sq = sqlite3.connect("video_chat.db")
+    cur = sq.cursor()
+    res = cur.execute("SELECT name FROM sqlite_master WHERE name='participant'")
+    if res.fetchone() is None:
+        cur.execute("CREATE TABLE participant(name, ip, login_time, logout_time)")
+
+    res = cur.execute("SELECT name FROM participant")
+    if name not in res.fetchall():
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        insert = """INSERT INTO participant(name, ip, login_time, logout_time) VALUES (?, ?, ?, ?);"""
+        data_tuple = (name, ip, current_time, "")
+        cur.execute(insert, data_tuple)
+        sq.commit()
+        res = cur.execute("SELECT * FROM participant")
+        print("*****SQL******\n", res.fetchall())
+
+
+Thread(target=accept_connections, args=(vid_server_socket, vid_clients,)).start()
+Thread(target=accept_connections, args=(aud_server_socket, aud_clients,)).start()
