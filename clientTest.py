567b0f7ff77523bbab276988270d132809fede56
Space Dinosaur <91723941+space-dino@users.noreply.github.com>
2024-05-16 22:19:54 +0300

    tests5gui

--- clientTest.py
+++ clientTest.py
@@ -173,7 +173,7 @@ class Client:
     def mainloop(self):
         self.root.mainloop()
 
-    def draw_GUI_frame(self, frame, cpos, fps_text):
+    """def draw_GUI_frame(self, frame, cpos, fps_text):
         cv2.putText(frame, fps_text, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
         cv2.putText(frame, f"{self.username}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -187,7 +187,22 @@ class Client:
             label.grid(row=0, column=cpos)
             self.labels.append(label)
             self.labels[cpos].configure(image=imgtk)
-            self.labels[cpos].image = imgtk
+            self.labels[cpos].image = imgtk"""
+
+    def draw_GUI_frame(self, frame, index, fps=None):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = Image.fromarray(frame)
+        frame = tk.CTkImage(light_image=frame, size=(400, 300))
+        while index >= len(self.labels):
+            label = tk.CTkLabel(self.root, text="")
+            self.labels.append(label)
+        self.labels[index].grid(row=index, column=0)
+        self.labels[index].configure(image=frame)
+        self.labels[index].image = frame
+        self.index_label.configure(text="client " + str(self.my_index) + " " + str(index))
+        if fps:
+            self.fps_label.configure(text=fps)
+        self.root.update()
 
 if __name__ == "__main__":
     client = Client()
