826f9f2f894d631820c85d9db723d57bb4a6bed6
Space Dinosaur <91723941+space-dino@users.noreply.github.com>
2024-06-21 21:18:39 +0300

    cligpt2 test

---
+++ protocolGPT2.py
@@ -0,0 +1,88 @@
+import pickle
+import socket
+import struct
+
+payload_size = struct.calcsize("Q")  # unsigned Long Long = 8 bytes
+index_size = struct.calcsize("I")  # unsigned Int = 4 bytes
+
+
+def send_frame(soc: socket, frame, flag: int, index: int, my_index: int):
+    data = pickle.dumps(frame)
+    message = struct.pack("I", index) + struct.pack("I", my_index) + struct.pack("I", flag) + struct.pack("Q",
+                                                                                                          len(data)) + data
+
+    try:
+        soc.sendall(message)
+    except TypeError:
+        print("connection closed")
+
+    # Packet Structure:
+    """ 
+
+    index - 4 bytes unsigned int
+    my_index - 4 bytes unsigned int
+    flag - 4 bytes unsigned int (0: video, 1: audio, 2: chat)
+    data_length - 8 bytes unsigned long long
+    data - data_length bytes data
+
+    """
+
+
+def receive_frame(soc: socket, data: bytes):
+    packed_index, data = receive_parameter(soc, data, index_size)  # index
+    index = struct.unpack("I", packed_index)[0]
+
+    packed_my_index, data = receive_parameter(soc, data, index_size)  # my_index
+    my_index = struct.unpack("I", packed_my_index)[0]
+
+    packed_flag, data = receive_parameter(soc, data, index_size)  # flag
+    flag = struct.unpack("I", packed_flag)[0]
+
+    packed_msg_size, data = receive_parameter(soc, data, payload_size)  # data_length
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+    frame_data, data = receive_parameter(soc, data, msg_size)  # data
+    frame = pickle.loads(frame_data)
+
+    return frame, data, flag, index, my_index
+
+
+def send_credentials(soc: socket, signup: bool, username: str, password: str):
+    sign_char = 'l'
+    if signup:
+        sign_char = 's'
+    message = sign_char + str(len(username)).zfill(4) + username + str(len(password)).zfill(4) + password
+    print(message)
+
+    try:
+        soc.sendall(message.encode())
+    except TypeError:
+        print("connection closed")
+
+
+def recv_credentials(soc: socket):
+    signup = soc.recv(1)
+    print(signup.decode())
+    ul = int(soc.recv(4))
+    username = soc.recv(ul)
+    pl = int(soc.recv(4))
+    password = soc.recv(pl)
+
+    print("ul:", ul, "username:", username)
+
+    if signup.decode() == 'l':
+        signup = False
+    else:
+        signup = True
+
+    return signup, username.decode(), password.decode()
+
+
+def receive_parameter(soc: socket, data, size):
+    while len(data) < size:
+        data += soc.recv(4 * 4096)
+
+    parameter = data[:size]
+    data = data[size:]
+
+    return parameter, data
---
+++ serGPT2.py
@@ -0,0 +1,237 @@
+import socket
+from threading import Thread
+import protocol4
+import sqlite3
+import select
+from PIL import Image
+import bcrypt
+
+vid_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+aud_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+login_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = '0.0.0.0'
+port = 9997
+vid_socket_address = (host, port)
+aud_socket_address = (host, port + 1)
+login_socket_address = (host, port + 2)
+
+vid_server_socket.bind(vid_socket_address)
+aud_server_socket.bind(aud_socket_address)
+login_server_socket.bind(login_socket_address)
+
+vid_server_socket.listen()
+aud_server_socket.listen()
+login_server_socket.listen()
+
+print(socket.gethostname())
+print("Listening video at", vid_socket_address, "audio at", aud_socket_address, "login at", login_socket_address)
+
+vid_clients = []
+aud_clients = []
+login_clients = []
+valid_addresses = []
+
+
+class connection:
+    def __init__(self, soc: socket.socket, index: int):
+        self.soc = soc
+        self.frame = b''
+        self.data = b''
+        self.index = index
+
+
+def accept_connections(soc: socket.socket, lis):
+    while True:
+        client_socket, addr = soc.accept()
+        cli_index = int(str(addr[0]).replace(".", ""))
+        con = connection(client_socket, cli_index)
+        lis.append(con)
+
+        if lis == login_clients:
+            print(f"GOT LOGIN CONNECTION FROM: ({addr[0]}:{addr[1]}) {cli_index}\n")
+
+        if lis == vid_clients:
+            print(f"GOT VIDEO CONNECTION FROM: ({addr[0]}:{addr[1]}) {cli_index}\n")
+
+        if lis == aud_clients:
+            print(f"GOT AUDIO CONNECTION FROM: ({addr[0]}:{addr[1]}) {cli_index}\n")
+
+        Thread(target=handle_client, args=(con, lis,)).start()
+
+
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed.decode('utf-8')
+
+
+def check_password(stored_password, plain_password):
+    return bcrypt.checkpw(plain_password.encode('utf-8'), stored_password.encode('utf-8'))
+
+
+def handle_client(con: connection, client_list):
+    login_finished = False
+
+    while True:
+        try:
+            readable, _, _ = select.select([con.soc], [], [], 1.0)
+            if readable and not login_finished:
+                if client_list == login_clients:
+                    signup, username, password = protocol4.recv_credentials(con.soc)
+                    print(signup, username, password)
+                    if signup:
+                        if handle_signup(username, password):
+                            con.soc.sendall("signup_success".encode())
+                        else:
+                            con.soc.sendall("signup_fail".encode())
+                    else:
+                        res = handle_login(username, password)
+                        if res == "True":
+                            valid_addresses.append(con.index)
+                            con.soc.sendall("login_success".encode())
+                            login_finished = False
+                            con.soc.close()
+                        elif res == "Wrong Username":
+                            con.soc.sendall("login_failU".encode())
+                        elif res == "Wrong Password":
+                            con.soc.sendall("login_failP".encode())
+                else:
+                    if con.index in valid_addresses:
+                        con.frame, con.data, *_ = protocol4.receive_frame(con.soc, con.data)
+                        if con in aud_clients:
+                            broadcast(con, aud_clients)
+                        else:
+                            broadcast(con, vid_clients)
+        except (ConnectionResetError, socket.error) as e:
+            print(f"Connection error: {e}")
+            remove_client(con, client_list)
+            break
+        except ValueError as e:
+            print(f"Login removed")
+            remove_client(con, client_list)
+            break
+
+
+def handle_signup(username, password):
+    # Connect to the database
+    sq = sqlite3.connect("video_chat.db")
+    cur = sq.cursor()
+
+    try:
+        # Check if the username already exists in the users table
+        cur.execute("SELECT 1 FROM users WHERE username = ?", (username,))
+        if cur.fetchone() is not None:
+            return False
+
+        # Insert the new user into the users table
+        cur.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hash_password(password)))
+        sq.commit()
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+        return False
+    finally:
+        # Ensure the cursor and connection are closed properly
+        cur.close()
+        sq.close()
+
+    return True
+
+
+def handle_login(username, password):
+    sq = sqlite3.connect("video_chat.db")
+    cur = sq.cursor()
+
+    # Check if the username exists in the users table
+    cur.execute("SELECT 1 FROM users WHERE username = ?", (username,))
+    if cur.fetchone() is None:
+        return "Wrong Username"
+
+    cur.execute("SELECT password FROM users WHERE username=?", (username,))
+    stored_password = cur.fetchone()
+    cur.close()
+    sq.close()
+    if stored_password:
+        if check_password(stored_password[0], password):
+            return "True"
+    return "Wrong Password"
+
+
+"""def broadcast(con, client_list):
+    for client in client_list:
+        if client != con:
+            ipos = get_index_pos(client)
+            cpos = get_index_pos(con)
+            try:
+                protocol4.send_frame(client.soc, con.frame, 0, cpos, ipos)
+            except (BrokenPipeError, ConnectionResetError, socket.error) as e:
+                print(f"Error broadcasting frame: {e}")
+                remove_client(client, client_list)"""
+
+
+def broadcast(con, client_list):  # inverse broadcast --- for every con, send to con every other clients' image
+    imm_arr = []
+
+    for client in client_list:
+        if client != con:
+            ipos = get_index_pos(con)
+            cpos = get_index_pos(client)
+            try:
+                imm_arr.append(client.frame)
+            except (BrokenPipeError, ConnectionResetError, socket.error) as e:
+                print(f"Error broadcasting frame: {e}")
+                remove_client(con, client_list)
+
+    comb_frame = combine_vertically(imm_arr)
+    protocol4.send_frame(con.soc, comb_frame, 0, cpos, 0)
+
+def combine_vertically(images):
+    # Determine the maximum width and total height of the combined image
+    max_width = max(img.width for img in images)
+    total_height = sum(img.height for img in images)
+
+    # Create a new image with white background (you can change this to any color)
+    combined_image = Image.new('RGB', (max_width, total_height), color='white')
+
+    # Paste each image into the combined image
+    y_offset = 0
+    for img in images:
+        combined_image.paste(img, (0, y_offset))
+        y_offset += img.height
+
+    return combined_image
+
+def get_index_pos(i):
+    sorted_numbers = sorted([conn.index for conn in vid_clients])
+    pos = sorted_numbers.index(i.index)
+    return pos
+
+
+def remove_client(con: connection, lis):
+    for i in lis:
+        if i.index == con.index:
+            print(f"Removing Connection {i.index}")
+            lis.remove(i)
+
+            for o in range(0, len(lis)):
+                if lis[o] is None:
+                    if o < len(lis) - 1:
+                        lis[o].index -= 1
+
+
+def create_users_table():
+    sq = sqlite3.connect("video_chat.db")
+    cur = sq.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS users (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username TEXT UNIQUE NOT NULL,
+                        password TEXT NOT NULL)''')
+    sq.commit()
+    cur.close()
+    sq.close()
+
+
+create_users_table()
+Thread(target=accept_connections, args=(vid_server_socket, vid_clients,)).start()
+Thread(target=accept_connections, args=(aud_server_socket, aud_clients,)).start()
+Thread(target=accept_connections, args=(login_server_socket, login_clients,)).start()
